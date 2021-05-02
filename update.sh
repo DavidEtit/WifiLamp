6fd7b8bcbc576e5048ac137c807a302bd445d4b4
flo <flo@pop-os.localdomain>
2021-05-02 17:27:39 +0200

    d

--- update.sh
+++ update.sh
@@ -10,21 +10,24 @@ if [ -d /home/pi/Backup/WifiLamp/ ]
 	then
 		sudo rm -r /home/pi/WifiLamp
 		echo "deleted old repo"
+		cd /home/pi/Backup/WifiLamp
+		sudo git pull
+		cd
 		sudo cp -r /home/pi/Backup/WifiLamp /home/pi
 		echo "copied backup repo"
 	else
 		echo "cant finde backup : pls check update.sh"
 fi
  
-if [ -d $repoPath ]
-	then
-		cd $repoPath
-		sudo git pull
-		echo "program is up to date"
-		cd
-	else
-		echo "invalide repo path : pls update update.sh"
-fi
+#if [ -d $repoPath ]
+#	then
+#		cd $repoPath
+#		sudo git pull
+#		echo "program is up to date"
+#		cd
+#	else
+#		echo "invalide repo path : pls update update.sh"
+#fi
 
 #start program
 if [ -e $progrmPath ]
